b64d2ae0a4fed3176d12b3481e6c6db32f34d104
zhjlydia <zhjlydia@foxmail.com>
2024-05-31 15:58:53 +0800

    feat: update dependency

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const webpack = require('webpack');
+const webpack = require('webpack')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -25,8 +25,7 @@ module.exports = {
         use: [
           isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
-          'postcss-loader',
-        ]
+        ],
       },
       {
         test: /\.less$/, //匹配所有的 less 文件
@@ -35,69 +34,68 @@ module.exports = {
         use: [
           isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
-          'postcss-loader',
-          'less-loader'
-        ]
+          'less-loader',
+        ],
       },
       {
         test: /\.(ts|tsx)$/,
         include: [path.resolve(__dirname, '../src')],
         enforce: 'pre',
-        use: ['thread-loader', 'babel-loader']
+        use: ['thread-loader', 'babel-loader'],
       },
       {
-        test:/\.(png|jpg|jpeg|gif|svg)$/,
-        type: "asset",
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset',
         parser: {
           //转base64的条件
           dataUrlCondition: {
             maxSize: 10 * 1024, // 10kb
-          }
+          },
         },
-        generator:{ 
-          filename:'static/images/[name].[contenthash:6][ext]'
-        },
-      },
-      {
-        test:/\.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
-        type: "asset", // type选择asset
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024, // 小于10kb转base64位
-          }
-        },
-        generator:{ 
-          filename:'static/fonts/[name].[contenthash:6][ext]', // 文件输出目录和命名
+        generator: {
+          filename: 'static/images/[name].[contenthash:6][ext]',
         },
       },
       {
-        test:/\.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
-        type: "asset", // type选择asset
+        test: /\.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
+        type: 'asset', // type选择asset
         parser: {
           dataUrlCondition: {
             maxSize: 10 * 1024, // 小于10kb转base64位
-          }
+          },
         },
-        generator:{ 
-          filename:'static/media/[name].[contenthash:6][ext]', // 文件输出目录和命名
+        generator: {
+          filename: 'static/fonts/[name].[contenthash:6][ext]', // 文件输出目录和命名
         },
-      }
-    ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
+        type: 'asset', // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 小于10kb转base64位
+          },
+        },
+        generator: {
+          filename: 'static/media/[name].[contenthash:6][ext]', // 文件输出目录和命名
+        },
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.tsx', '.ts'],
     alias: {
-      '@': path.resolve(__dirname, '../src')
+      '@': path.resolve(__dirname, '../src'),
     },
     modules: [path.resolve(__dirname, '../node_modules')],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
-      inject: true
+      inject: true,
     }),
     new webpack.DefinePlugin({
-      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV)
+      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV),
     }),
   ],
   // 开启webpack持久化存储缓存
